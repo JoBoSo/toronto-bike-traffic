5e80738aeda9ecd272d9928539f7b65aff1b46be
JoBoSo <jamesbscott@hotmail.ca>
2025-12-12 16:36:27 -0500

    added popup info to counters

--- next-app/app/components/Map/MapClient/hooks/BaseLayers/useRenderCounterLocations.ts
+++ next-app/app/components/Map/MapClient/hooks/BaseLayers/useRenderCounterLocations.ts
@@ -8,6 +8,7 @@ export function useRenderCounterLocations(
   useEffect(() => {
     if (mapInstance && data && data.length > 0) {
       console.log("Rendering counter locations");
+      console.log(data[0])
       
       mapInstance.createPane("counterLocationsPane");
       mapInstance.getPane("counterLocationsPane")!.style.zIndex = "630";
@@ -15,25 +16,26 @@ export function useRenderCounterLocations(
       const layer = L.geoJSON(data, {
         pane: "counterLocationsPane", // assign layer to pane
         onEachFeature: (feature, layer) => {
-          layer.bindPopup("");
-          layer.on("popupopen", (e) => {
-            e.popup.setContent(
-              feature.properties.location_dir_id.toString() +
-                ", " +
-                feature.properties.location_name.toString()
-            );
-          });
+          const location_dir_id = feature.properties.location_dir_id.toString()
+          const location_name = feature.properties.location_name.toString()
+          const direction = feature.properties.direction.toString()
+          const first_active = feature.properties.first_active.toString()
+          const last_active = feature.properties.last_active.toString()
+          layer.bindPopup(`
+            <b>${location_name} (#${location_dir_id})</b>
+            <p style="margin: 2px 0;">${direction}</p>
+            <p style="margin: 2px 0;">Active from ${first_active} to ${last_active}</p>
+          `);
         },
         pointToLayer: (feature, latlng) => {
           const circle = L.circleMarker(latlng, {
-            pane: "counterLocationsPane", // assign circle to pane
+            pane: "counterLocationsPane",
             radius: 4,
             color: "rgba(48, 48, 48, 1)",
             fillColor: "rgba(48, 48, 48, 1)",
             fillOpacity: 1,
             weight: 1,
           });
-          // circle.bringToFront(); // ensures this circle is above other layers
           return circle;
         },
       });
