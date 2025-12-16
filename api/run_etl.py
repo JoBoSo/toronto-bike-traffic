59c20cb20df7a35f961b478838cea1f7a6dd13b4
JoBoSo <jamesbscott@hotmail.ca>
2025-12-16 11:58:23 -0500

    cleaned up ETL runner

--- api/run_etl.py
+++ api/run_etl.py
@@ -4,22 +4,36 @@ import asyncio
 
 async def main():
     print("Starting ETL process")
+
     bcl = BicycleCountersLoader()
-    await bcl.counter_locations_to_geojson()
-    await bcl.counts_daily_to_parquet()
-    await bcl.counts_15m_to_parquet()
-    # await bcl.load_counter_locations_into_sqlite()
-    # bcl.load_location_groups_into_sqlite()
-    # await bcl.load_daily_counts_into_sqlite()
-    # await bcl.load_15m_counts_into_sqlite()
-    # bcl.load_annual_counts_into_sqlite()
-    # bcl.load_monthly_counts_into_sqlite()
-    # bcl.load_hourly_counts_into_sqlite()
-    # bcl.load_location_group_stats_overall_into_sqlite()
-    # bcl.load_location_group_stats_yearly_into_sqlite()
-    # bcl.load_location_group_stats_monthly_into_sqlite()
-    # bcl.load_location_group_stats_weekly_into_sqlite()
-    # bcl.load_fifteen_min_counts_by_year_and_month_into_sqlite()
+
+    async def data_files_use_in_app():
+        print("Perparing data files used in app")
+        await bcl.counter_locations_to_geojson()
+        await bcl.counts_daily_to_parquet()
+        await bcl.counts_15m_to_parquet()
+        print("Finished perparing data files used in app")
+
+    async def data_files_not_use_in_app():
+        print("Perparing data files not used in app")
+        await bcl.load_counter_locations_into_sqlite()
+        bcl.load_location_groups_into_sqlite()
+        await bcl.load_daily_counts_into_sqlite()
+        await bcl.load_15m_counts_into_sqlite()
+        bcl.load_annual_counts_into_sqlite()
+        bcl.load_monthly_counts_into_sqlite()
+        bcl.load_hourly_counts_into_sqlite()
+        bcl.load_location_group_stats_overall_into_sqlite()
+        bcl.load_location_group_stats_yearly_into_sqlite()
+        bcl.load_location_group_stats_monthly_into_sqlite()
+        bcl.load_location_group_stats_weekly_into_sqlite()
+        bcl.load_fifteen_min_counts_by_year_and_month_into_sqlite()
+        print("Finished perparing data files not used in app")
+
+    await data_files_use_in_app()
+    # await data_files_not_use_in_app()
+
+    print('Finished ETL process')
 
 if __name__ == "__main__":
     asyncio.run(main())
